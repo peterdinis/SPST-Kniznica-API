c57d9d944eb30425736c92b9221b57a38ecfbf23
Peterdinis <peterdinis0@pobox.sk>
2023-02-01 20:31:45 +0100

    feat: Finish book crud

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -1,11 +1,12 @@
 import express from "express";
-import { createBookFn, deleteBookFn, displayAllBooksFn, displayOneBookFn, updateBookFn } from "../controllers/bookController";
+import { createBookFn, deleteBookFn, displayAllBooksFn, displayOneBookFn, searchingForBookFn, updateBookFn } from "../controllers/bookController";
 
 const router = express.Router();
 
 router.get("/books", displayAllBooksFn);
 router.get("/book/:id", displayOneBookFn);
 router.post("/books", createBookFn);
+router.get("/books/search/:name", searchingForBookFn);
 router.put("/book/:id", updateBookFn);
 router.delete("/book/:id", deleteBookFn);
 
