ad66896e1b54cf46a07e93d4d34af1b0b0ae4238
Peterdinis <peterdinis0@pobox.sk>
2023-02-01 17:57:11 +0100

    feat: Working on API

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,12 @@
 import express, {Application} from "express";
+import cors from "cors";
+import dotenv from "dotenv";
 
 const app: Application = express();
+
+app.use(cors());
+app.use(express.json());
+dotenv.config();
 
 const PORT = process.env.PORT as unknown as number;
 
