19f6b2a2a9b38dd405e4bdbe48ded6c863d383b9
Peterdinis <peterdinis0@pobox.sk>
2023-02-03 13:05:57 +0100

    feat: Adding helper middlewares

---
+++ src/middleware/errorHandler.ts
@@ -0,0 +1,13 @@
+import { Response } from "express";
+
+function errorHandler(err: { message: string; stack: string }, res: Response) {
+  /* eslint-enable no-unused-vars */
+  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  res.status(statusCode);
+  res.json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? "🥞" : err.stack,
+  });
+}
+
+export default errorHandler;
---
+++ src/schemas/userSchema.ts
@@ -0,0 +1,5 @@
+import { z } from "zod";
+
+export const createRegisterSchema = z.object({});
+
+export const loginUserSchema = z.object({});
