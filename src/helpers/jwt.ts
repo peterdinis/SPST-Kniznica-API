a37940ad8903c281def6b90a4c0c1841bf16245e
Peterdinis <peterdinis0@pobox.sk>
2023-02-03 14:29:40 +0100

    feat: Create helper jwt file

--- src/helpers/jwt.ts
+++ src/helpers/jwt.ts
@@ -1,9 +1,36 @@
-import * as jwt from "jsonwebtoken"
+import * as jwt from "jsonwebtoken";
 import { IUser } from "../interfaces/IUser";
 
-function generateAccessToken(user: IUser) {
-    return jwt.sign({ userId: user.id }, process.env.JWT_ACCESS_SECRET as string, {
-      expiresIn: '5m',
-    });
-  }
+export function generateAccessToken(user: IUser) {
+  return jwt.sign(
+    { userId: user.id },
+    process.env.JWT_ACCESS_SECRET as string,
+    {
+      expiresIn: "5m",
+    }
+  );
+}
+
+export function generateRefreshToken(user: IUser, tokId: number) {
+  return jwt.sign(
+    {
+      userId: user.id,
+      tokId,
+    },
+    process.env.JWT_REFRESH_SECRET as string,
+    {
+      expiresIn: "4h",
+    }
+  );
+}
+
+
+export function generateTokens(user: IUser, tokId: number) {
+    const accessToken = generateAccessToken(user);
+    const refreshToken = generateRefreshToken(user, tokId);
   
+    return {
+      accessToken,
+      refreshToken,
+    };
+}
