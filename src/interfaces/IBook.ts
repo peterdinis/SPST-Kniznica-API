7ee471789e95c0c61a411259553c5aac0774dfbc
Peterdinis <peterdinis0@pobox.sk>
2023-02-03 12:45:19 +0100

    feat: New files wip

---
+++ src/interfaces/IBook.ts
@@ -0,0 +1,10 @@
+export interface IBook {
+    id?: number;
+    name: string;
+    description: string;
+    image: string;
+    author: string;
+    status: string;
+    pages: number;
+    year: number;
+}
