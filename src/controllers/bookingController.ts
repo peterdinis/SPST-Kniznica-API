c3be7984dead26a2815ba65df622b8a85193be39
Peterdinis <peterdinis0@pobox.sk>
2023-02-05 17:53:13 +0100

    FOO

---
+++ src/controllers/bookingController.ts
@@ -0,0 +1,13 @@
+import { Request, Response } from "express";
+
+export const myBorrowedBooks = (req: Request, res: Response) => {
+    return;
+}
+
+export const createNewOrder = (req: Request, res: Response) => {
+    return;
+}
+
+export const returnOrder = (req: Request, res: Response) => {
+    return;
+}
