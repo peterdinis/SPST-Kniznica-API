37dc679464d2646bdb8df8a3eb1a9728269d5ad3
Peterdinis <peterdinis0@pobox.sk>
2023-02-05 12:24:05 +0100

    feat: Updating student auth

---
+++ src/middleware/isLogged.ts
@@ -0,0 +1,28 @@
+import * as jwt from "jsonwebtoken";
+import {NextFunction,  Response} from "express";
+import { getErrorMessage } from "../helpers/catchErrorMessage";
+
+function isLogged(req: any, res: Response, next: NextFunction) {
+    const { authorization } = req.headers;
+
+  if (!authorization) {
+    res.status(401);
+    throw new Error('🚫 Un-Authorized 🚫');
+  }
+
+  try {
+    const token = authorization.split(' ')[1];
+    const payload = jwt.verify(token, process.env.JWT_ACCESS_SECRET as string);
+    req.payload = payload;
+  } catch (err: any) {
+    res.status(401);
+    if (err.name === 'TokenExpiredError') {
+      getErrorMessage(err);
+    }
+    throw new Error('🚫 Un-Authorized 🚫');
+  }
+
+  return next();
+}
+
+export default isLogged
--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -1,9 +1,13 @@
 import express from "express";
-import { loginStudent, registerStudent } from "../controllers/studentController";
+import { loginStudent, profileFn, refreshTokenFn, registerStudent, revokeRefreshTokenFn } from "../controllers/studentController";
+import isLogged from "../middleware/isLogged";
 
 const router = express.Router();
 
 router.post("/student/register", registerStudent);
 router.post("/student/login", loginStudent);
+router.post("/token/refresh", refreshTokenFn);
+router.post("/token/revoke", revokeRefreshTokenFn);
+router.get("/student/profile", isLogged, profileFn);
 
 export default router;
