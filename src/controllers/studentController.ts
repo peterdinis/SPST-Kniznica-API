9278922bac6db626af7436324a3e0fdb8553fe2c
Peterdinis <peterdinis0@pobox.sk>
2023-02-05 11:42:15 +0100

    fix: Fixing problem with password hashing

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,71 +1,51 @@
-import {Request, Response} from "express";
-import bcrypt from "bcrypt";
+import { Request, Response } from "express";
 import { getErrorMessage } from "../helpers/catchErrorMessage";
 import db from "../helpers/db";
-import hashToken from "../helpers/hashToken";
-
-const addRefreshTokenToWhiteList = (tokId: string, refreshToken: string, userId: string) => {
-    return db.refreshToken.create({
-        data: {
-            id: tokId,
-            hashedToken: hashToken(refreshToken),
-            userId
-        }
-    })
-}
+import bcrypt from "bcryptjs";
 
 export const registerStudent = async (req: Request, res: Response) => {
-        const {name, lastName, email, password, role} = req.body;
+  const { name, lastName, email, password, role } = req.body;
 
-        if(!name || !lastName || !email || !password || !role) {
-            res.status(400);
-            throw new Error("All fields are required");
-        }
+  if (!name || !lastName || !email || !password || !role) {
+    res.status(400);
+    throw new Error("All fields are required");
+  }
 
-        const existingStudent = await db.user.findUnique({
-            where: {
-                email
-            }
-        })
+  const existingStudent = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
 
-        if(existingStudent) {
-            res.status(400);
-            throw new Error("Student already exists");
-        }
+  if (existingStudent) {
+    res.status(400);
+    throw new Error("Student already exists");
+  }
 
-        if(password.length < 5) {
-            res.status(400);
-            throw new Error("Password must be at least 5 characters");
-        }
+  if (password.length < 5) {
+    res.status(400);
+    throw new Error("Password must be at least 5 characters");
+  }
 
-        const hashedPassword = bcrypt.hash(password, 10) as unknown as string;
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
 
-        const newStudent = await db.user.create({
-            data: {
-              name,
-              lastName,
-              email,
-              password: hashedPassword,
-              role,
-            }
-        })
+  const newStudent = await db.user.create({
+    data: {
+      name,
+      lastName,
+      email,
+      password: hashedPassword,
+      role,
+    },
+  });
 
-        console.log(newStudent);
-
-        /* const tokId = uuid() as unknown as number;
-        const {accessToken, refreshToken} = generateTokens(newStudent, tokId);
-        
-        return res.status(201).json({
-            student: newStudent,
-            accessToken,
-            refreshToken
-        }) */
-}
+  return res.status(201).json(newStudent);
+};
 
 export const loginStudent = (req: Request, res: Response) => {
-    try {
-
-    } catch (err) {
-        getErrorMessage(err);
-    }
-}
+  try {
+  } catch (err) {
+    getErrorMessage(err);
+  }
+};
