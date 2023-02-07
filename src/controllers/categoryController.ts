9255ca73ba8935957637d72e03212516b6900141
Peterdinis <peterdinis0@pobox.sk>
2023-02-07 19:08:08 +0100

    feat: Adding pagination to categories

--- src/controllers/categoryController.ts
+++ src/controllers/categoryController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from "express";
 import validate from "../schemas/validateSchema";
 import { createCategorySchema } from "../schemas/categorySchema";
 import db from "../helpers/db";
+import { getErrorMessage } from "../helpers/catchErrorMessage";
 
 export const displayAllCategoriesFn = async (req: Request, res: Response) => {
   const allCategories = await db.category.findMany();
@@ -23,6 +24,22 @@ export const categoryDetailsFn = async (req: Request, res: Response) => {
 
   return oneCategoy;
 };
+
+export const categoryPagination = async (req: Request, res: Response) => {
+  try {
+    const limitValue = req.query.limit ||2;
+    const skipValue = req.query.skip || 0;
+
+    const allCategories = await db.category.findMany({
+      take: limitValue as unknown as number,
+      skip: skipValue as unknown as number
+    })
+
+    return res.json(allCategories);
+  } catch (err) {
+    getErrorMessage(err);
+  }
+}
 
 export const createCategoryFn = async (req: Request, res: Response) => {
   validate(createCategorySchema);
--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -3,10 +3,12 @@ import {
   categoryDetailsFn,
   createCategoryFn,
   displayAllCategoriesFn,
+  categoryPagination
 } from "../controllers/categoryController";
 
 const router = express.Router();
 
+router.get("/categories/paginate", categoryPagination);
 router.get("/categories", displayAllCategoriesFn);
 router.get("/category/:id", categoryDetailsFn);
 router.post("/category", createCategoryFn);
