803630b44614e35f5bcf8f4d0bc63f5d23f1588b
Peterdinis <peterdinis0@pobox.sk>
2023-02-13 12:51:11 +0100

    feat: Adding new fn for booking book

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -1,49 +1,100 @@
 import db from "../helpers/db";
 import { Request, Response } from "express";
- 
+import validate from "../schemas/validateSchema";
+import { borrowBookSchema } from "../schemas/bookingSchema";
+import { AVAIABLE, NONAVAIABLE } from "../constants/bookStatus";
+
 export const findAllBookings = async (req: Request, res: Response) => {
-    const findAllBookings = await db.booking.findMany({});
-    return findAllBookings;
-}
+  const findAllBookings = await db.booking.findMany({});
+  return findAllBookings;
+};
 
 export const findBookingDetails = async (req: Request, res: Response) => {
-    const {id} = req.params;
+  const { id } = req.params;
 
-    const findBookingDetail = await db.booking.findUnique({
-        where: {id: Number(id)},
-    })
+  const findBookingDetail = await db.booking.findUnique({
+    where: { id: Number(id) },
+  });
 
-    if(!findBookingDetail) {
-        return res.status(404).json({
-            message: "Booking with this id not found"
-        })
-    }
+  if (!findBookingDetail) {
+    return res.status(404).json({
+      message: "Booking with this id not found",
+    });
+  }
 
-    return findBookingDetail;
-}
+  return findBookingDetail;
+};
 
 export const displayMyBorrowedBooks = async (req: Request, res: Response) => {
-    const {email} = req.params;
-    
-    const findAllMyBorrowedBooks = await db.booking.findMany({
-        where: {
-            email
-        }
-    })
+  const { email } = req.params;
 
-    if(findAllMyBorrowedBooks.length === 0) {
-        return res.status(200).json({
-            message: "No borrowed books"
-        })
-    }
+  const findAllMyBorrowedBooks = await db.booking.findMany({
+    where: {
+      email,
+    },
+  });
 
-    return res.json(findAllMyBorrowedBooks);
-}
+  if (findAllMyBorrowedBooks.length === 0) {
+    return res.status(200).json({
+      message: "No borrowed books",
+    });
+  }
 
-export const createNewBooking = (req: Request, res: Response) => {
-    return;
-}
+  return res.json(findAllMyBorrowedBooks);
+};
+
+export const createNewBooking = async (req: Request, res: Response) => {
+  validate(borrowBookSchema);
+
+  const { email, bookId } = req.body;
+
+  const findStudentOrTeacher = await db.user.findUnique({
+    where: {
+      email,
+    },
+  });
+
+  if (!findStudentOrTeacher) {
+    res.status(404);
+    throw new Error(`Student or teacher not found`);
+  }
+
+  const findOneBook = await db.book.findFirst({
+    where: {
+      id: bookId,
+      status: AVAIABLE,
+    },
+  });
+
+  if (findOneBook!.status === NONAVAIABLE) {
+    return res.status(409).json({
+      message:
+        "Knihu si nemôžeme požičať lebo je nedostupná/knihu si niekto požičal",
+    });
+  }
+
+  const createNewBooking = await db.booking.create({
+    data: {
+      ...req.body,
+    },
+  });
+
+  await db.book.update({
+    where: {
+      id: bookId,
+    },
+
+    data: {
+      status: NONAVAIABLE,
+    },
+  });
+
+  return res.status(201).json({
+    book: createNewBooking,
+    message: "Nova objednávka bola vytvorená knihu máte dostupnú na 7 dní",
+  });
+};
 
 export const returnBooking = (req: Request, res: Response) => {
-    return;
-}
+  return;
+};
