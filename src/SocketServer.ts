8ddf74d2c2f36d5c5688f6fcd594439e377b7f71
Peterdinis <peterdinis0@pobox.sk>
2023-02-14 13:23:03 +0100

    feat: Updating socket server wip

--- src/SocketServer.ts
+++ src/SocketServer.ts
@@ -1,7 +1,22 @@
 import { Socket } from "socket.io";
+import { IUser } from "./interfaces/IUser";
+import { IMessage } from "./interfaces/IMessage";
 
 let users: Array<[]> = [];
 
 export const SocketServer = (socket: Socket) => {
+    
+    socket.on("registerUser", (user: IUser) => {
+
+    });
+
+    socket.on("disconnect", () => {
+
+    });
+
+    socket.on("writeMessage", (data: IMessage) => {
+
+    });
+
     
 }
