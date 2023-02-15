e50508fc9d57ab2716e5aca445f3af4315fe26ea
Peterdinis <peterdinis0@pobox.sk>
2023-02-15 20:35:34 +0100

    fix: Fixing logic in display my borrowed books endpoint

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -22,3 +22,25 @@ export const findBookingDetails = async (req: Request, res: Response) => {
   return res.json(findBookingDetail);
 };
 
+export const myBorrowedBooks = async (req: Request, res: Response) => {
+  const {id} = req.params;
+  
+  const findExistingUser = await db.user.findFirst({
+    where: {
+      id
+    }
+  })
+
+  if(!findExistingUser) {
+    res.status(404);
+    throw new Error("User with this id does not exist");
+  }
+
+  const userBorrowedBooks = await db.booking.findMany({
+    where: {
+      userId: id
+    }
+  })
+
+  return res.json(userBorrowedBooks);
+}
--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { findAllBookings, findBookingDetails } from "../controllers/bookingController";
+import { findAllBookings, findBookingDetails, myBorrowedBooks } from "../controllers/bookingController";
 
 const router = express.Router();
 
 router.get("/booking", findAllBookings);
 router.get("/booking/:id", findBookingDetails);
+router.get("/booking/me/:id", myBorrowedBooks)
 
 export default router;
