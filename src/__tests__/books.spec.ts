c25c6a78d94bc1d81e796f6c58d7198cf164c478
Peterdinis <peterdinis0@pobox.sk>
2023-02-15 14:46:23 +0100

    feat: Updating tests wip

--- src/__tests__/books.spec.ts
+++ src/__tests__/books.spec.ts
@@ -1,13 +1,17 @@
 import "mocha";
 import request from "supertest"
 import { app } from "../server";
-import { expect } from "chai";
+import chai, { expect } from "chai";
+
+chai.use(require('chai-like'));
+chai.use(require('chai-things'));
 
 describe("Test GET /books", () => {
     it("Display all books", async() => {
         const allBooksRequest = await request(app).get("/books")
         const allBooksResponse = allBooksRequest.body;
-        console.log(allBooksResponse.length)
+        expect(allBooksRequest.status).to.eq(200);
+        expect(allBooksResponse).to.be.an("array");
     })
 })
 
@@ -17,14 +21,14 @@ describe("Test GET /books/:id", () => {
         return;
     })
 
-    it("Trying to find book but id does not exists", async() => {
+    it.skip("Trying to find book but id does not exists", async() => {
         const testId = 3030;
 
         const allBooksRequest = await request(app).get(`/book/${testId}`)
         expect(allBooksRequest.status).to.equal(404);
     })
 
-    it("Trying to find book by id but requested id is not nmber", async() => {
+    it.only("Trying to find book by id but requested id is not nmber", async() => {
         const testId = "EOEOEOEOE"
 
         const allBooksRequest = await request(app).get(`/book/${testId}`)
