ca07f0df6e21127b8536e9ac1e0db67f2a8da61d
Peterdinis <peterdinis0@pobox.sk>
2023-02-17 15:22:50 +0100

    fix: Start to fixing socket.io wip

--- src/SocketServer.ts
+++ src/SocketServer.ts
@@ -1,7 +1,3 @@
 import { Socket } from "socket.io";
-import { IUser } from "./interfaces/IUser";
-import { IMessage } from "./interfaces/IMessage";
-
-let users: Array<[]> = [];
 
 export const SocketServer = (socket: Socket) => {}
--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, {Application} from "express";
+import express, { Application } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 import exampleRoute from "./routes/exampleRoute";
@@ -7,7 +7,7 @@ import categoryRoutes from "./routes/categoryRoutes";
 import studentRoutes from "./routes/studentRoutes";
 import morgan from "morgan";
 import helmet from "helmet";
-import bookingRoutes from "./routes/bookingRoutes"
+import bookingRoutes from "./routes/bookingRoutes";
 import cookieParser from "cookie-parser";
 import http from "http";
 import { SocketServer } from "./SocketServer";
@@ -15,32 +15,38 @@ import { Server, Socket } from "socket.io";
 
 export const app: Application = express();
 
-app.use(cors({
+app.use(
+  cors({
     origin: true,
     methods: "*",
-    credentials: true
-}));
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
 app.use(cookieParser());
-app.use(express.urlencoded({ limit: '50mb', extended: true }));
+app.use(express.urlencoded({ limit: "50mb", extended: true }));
 dotenv.config();
 
 /* Socket.io initialization */
-const server = http.createServer(app);
+/* const server = http.createServer(app);
 const io = new Server(server, {
-    pingTimeout: 60000,
-    cors: {
-        origin: true,
-    }
-})
+  pingTimeout: 60000,
+  cors: {
+    origin: "http://localhost:3000"
+  },
+  path: "/socket"
+});
 
 io.on("connection", (socket: Socket) => {
-    console.log("Connection work");
-    SocketServer(socket);
-})
+  console.log("Connection work");
+  console.log(`⚡: ${socket.id} user just connected!`);
+  io.on("disconnect", () => {
+    console.log("🔥: A user disconnected");
+  });
+}); */
 
 const PORT = process.env.PORT as unknown as number;
 
@@ -51,5 +57,5 @@ app.use(studentRoutes);
 app.use(bookingRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Applikácia beží na porte ${PORT}`);
-})
+  console.log(`Applikácia beží na porte ${PORT}`);
+});
