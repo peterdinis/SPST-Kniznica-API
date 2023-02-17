55b809755bcccf0c5625642b4f98ad139f4f659d
Peterdinis <peterdinis0@pobox.sk>
2023-02-17 14:50:01 +0100

    feat: Create return book fn

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -103,7 +103,7 @@ export const createNewBooking = async (req: Request, res: Response) => {
 
 
 
-  return res.json({
+  return res.status(201).json({
     message: "Požičanie knihy bolo úspesšné",
     requestedBook: findBorrowedBook!.name,
     orderInfo: newOrder
@@ -111,7 +111,42 @@ export const createNewBooking = async (req: Request, res: Response) => {
 }
 
 export const returnBook = async (req: Request, res: Response) => {
-  return;
+  const {id, bookId, userId} = req.body;
+
+  const findStudentById = await db.user.findFirst({
+    where: {
+      id: userId
+    }
+  })
+
+  if(!findStudentById) {
+    res.status(404);
+    throw new Error("Student with this id does not exist");
+  }
+
+  const findBookById = await db.book.findUnique({
+    where: {
+      id: Number(bookId) as any,
+    }
+  })
+
+  if(!findBookById) {
+    res.status(404);
+    throw new Error("Book with this id does not exist")
+  }
+
+  const returnBook = await db.booking.delete({
+    where: {
+      id
+    }
+  })
+
+  console.log(returnBook);
+
+  return res.status(200).json({
+    message: "Kniha bola vrátená",
+    requestedBook: findBookById.name,
+  })
 }
 
 export const extensionForBook = (req: Request, res: Response) => {
