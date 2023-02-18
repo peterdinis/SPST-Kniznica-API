a34b06a0d91c3b2623eefa8cbe958b8416bbddad
Peterdinis <peterdinis0@pobox.sk>
2023-02-18 16:10:06 +0100

    test: Updating e2e tests

--- src/__tests__/books.spec.ts
+++ src/__tests__/books.spec.ts
@@ -1,16 +1,21 @@
 import "mocha";
-import request from "supertest"
+import request from "supertest";
 import { app } from "../server";
-import chai, { expect } from "chai";
+import chai, { expect, should } from "chai";
 
-chai.use(require('chai-like'));
-chai.use(require('chai-things'));
+chai.use(require("chai-like"));
+chai.use(require("chai-things"));
 
 describe("Test GET /books", () => {
-    it("Display all books", async() => {
-        const allBooksRequest = await request(app).get("/books")
+  it("Display all books", async (done) => {
+    const allBooksRequest = await request(app)
+      .get("/books")
+      .end((res, err) => {
+        if(err) throw err;
         const allBooksResponse = allBooksRequest.body;
-        expect(allBooksRequest.status).to.eq(200);
+        res.should.have.status(200);
         expect(allBooksResponse).to.be.an("array");
-    })
-})
+        done();
+      });
+  });
+});
--- src/__tests__/example.spec.ts
+++ src/__tests__/example.spec.ts
@@ -1,14 +1,14 @@
 import "mocha";
-import request from "supertest"
+import request from "supertest";
 import { app } from "../server";
 import { expect } from "chai";
 
 describe("Test example endpoint", () => {
-    it("Get return value from our example endpoint", async() => {
-        const exampleRequest = await request(app).get("/example");
-        const exampleResponse = exampleRequest.text;
-        
-        expect(exampleRequest.status).to.equal(200);
-        expect(exampleResponse).to.equal("OK");
-    })
-})
+  it("Get return value from our example endpoint", async () => {
+    const exampleRequest = await request(app).get("/example");
+    const exampleResponse = exampleRequest.text;
+
+    expect(exampleRequest.status).to.equal(200);
+    expect(exampleResponse).to.equal("OK");
+  });
+});
