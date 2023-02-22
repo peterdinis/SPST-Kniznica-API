3971860aaa4cfdb9be8d10d23afd51baf3fee7b8
Peterdinis <peterdinis0@pobox.sk>
2023-02-22 10:47:56 +0100

    feat: Adding new endpoints in uploadController

--- src/middleware/cloudinary.ts
+++
@@ -1,9 +0,0 @@
-import cloudinary from "cloudinary";
-
-const cloudinaryConfig = cloudinary.v2.config({
-  cloud_name: process.env.CLOUD_NAME,
-  api_key: process.env.CLOUD_API_KEY,
-  api_secret: process.env.STORAGE_API_SECRET,
-});
-
-export default cloudinaryConfig;
--- src/routes/uploadRoutes.ts
+++ src/routes/uploadRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { checkAccountInfo, checkGoFileServerStatus } from "../controllers/uploadController";
+import { checkAccountInfo, checkGoFileServerStatus, uploadPictureForStudent, uploadPictureForTeacher } from "../controllers/uploadController";
 
 const router = express.Router();
 
 router.get("/upload/account/status", checkAccountInfo);
 router.get("/upload/server/status", checkGoFileServerStatus);
+router.patch("/upload/:id/new", uploadPictureForStudent);
+router.patch("/upload/teacher/:id/new", uploadPictureForTeacher);
 
 export default router;
