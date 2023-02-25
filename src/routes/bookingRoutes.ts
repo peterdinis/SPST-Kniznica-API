8a918d189713206d9cb84d4e067d0ff8eafcb7c3
Peterdinis <peterdinis0@pobox.sk>
2023-02-25 10:37:17 +0100

    Feat: Writing new booking endpoints

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -1,9 +1,11 @@
 import express from "express";
-import { getMyBorrowedBooks, getMyTeacherBorrowedBooks } from "../controllers/bookingController";
+import { createNewBooking, displayAllBookings, getMyBorrowedBooks, getMyTeacherBorrowedBooks } from "../controllers/bookingController";
 
 const router = express.Router();
 
+router.get("/booking", displayAllBookings)
 router.get("/student/borrowed/:email", getMyBorrowedBooks);
 router.get("/teacher/borrowed/:email", getMyTeacherBorrowedBooks);
+router.post("/booking", createNewBooking);
 
 export default router;
