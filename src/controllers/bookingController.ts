7117a07cde649c6c15faae3ab909c1bc918734b6
Peterdinis <peterdinis0@pobox.sk>
2023-02-26 19:20:00 +0100

    feat: Adding new functions in booking

---
+++ src/controllers/bookingController.ts
@@ -0,0 +1,22 @@
+import {Request, Response} from "express";
+import db from "../helpers/db";
+
+export const getAllBooking = (req: Request, res: Response) => {
+    return;
+}
+
+export const bookingInfo = (req: Request, res: Response) => {
+    return;
+}
+
+export const getMyBooking = (req: Request, res: Response) => {
+    return;
+}
+
+export const createBooking = (req: Request, res: Response) => {
+    return;
+}
+
+export const returnBooking = (req: Request, res: Response) => {
+    return
+}
---
+++ src/schemas/bookingSchema.ts
@@ -0,0 +1,5 @@
+import {z} from "zod";
+
+export const createBookingSchema = z.object({});
+
+export const returnBookingSchema = z.object({});
