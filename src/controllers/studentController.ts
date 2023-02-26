a0e9fa7a088c6fd71a7efb6c239517beb2cd8a06
Peterdinis <peterdinis0@pobox.sk>
2023-02-26 16:37:28 +0100

    refactor: Updating student controller

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -1,2 +1,34 @@
 import { Request, Response } from "express";
 import db from "../helpers/db";
+
+export const getAllStudents = async (req: Request, res: Response) => {
+    const displayAllStudents = await db.student.findMany();
+    return res.json(displayAllStudents);
+}
+
+export const getStudentInfo = async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const findOneStudent = await db.student.findUnique({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    if(!findOneStudent){
+        return res.status(404).json("Student not found");
+    }
+
+    return findOneStudent;
+}
+
+export const studentRegister = async (req: Request, res: Response) => {
+    return;
+}
+
+export const studentLogin = async (req: Request, res: Response) => {
+    return;
+}
+
+export const studentProfile = async (req: Request, res: Response) => {
+    return;
+}
---
+++ src/interfaces/IStudent.ts
@@ -0,0 +1,11 @@
+export interface IStudent {
+    id: number;
+    name: string;
+    lastName: string;
+    username: string;
+    email: string;
+    password: string;
+    role: string;
+    createdAt: string | Date;
+    updatedAt: string | Date
+}
