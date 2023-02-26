7544a58f4f7576ce835d9af5ef4dd37a45004ee4
Peterdinis <peterdinis0@pobox.sk>
2023-02-26 18:17:19 +0100

    feat: Cleaning

--- src/controllers/teacherController.ts
+++ src/controllers/teacherController.ts
@@ -1,3 +1,7 @@
 import { Request, Response } from "express";
 import db from "../helpers/db";
 
+export const getAllTeachers = async (req: Request, res: Response) => {
+  const displayAllTeachers = await db.teacher.findMany();
+  return res.json(displayAllTeachers);
+};
