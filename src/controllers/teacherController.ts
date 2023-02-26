ac2a5e9ca0981721cf1240cb343e4d9d35e6b9b3
Peterdinis <peterdinis0@pobox.sk>
2023-02-26 16:17:20 +0100

    feat: Update booking

--- src/controllers/teacherController.ts
+++ src/controllers/teacherController.ts
@@ -1,39 +1,3 @@
 import { Request, Response } from "express";
 import db from "../helpers/db";
 
-export const displayAllTeacher = async (req: Request, res: Response) => {
-    const displayAllTeachers = await db.teacher.findMany();
-    return res.json(displayAllTeachers);
-}
-
-
-export const saveTeacher = async (req: Request, res: Response) => {
-    const newTeacher = await db.teacher.create({
-        data: {
-           ...req.body
-        }
-    })
-
-    return res.json(newTeacher);
-}
-
-export const removeTeacher = async (req: Request, res: Response) => {
-    const findTeacher = await db.teacher.findFirst({
-        where: {
-            email: String(req.query.email)
-        }
-    })
-
-    if(!findTeacher) {
-        res.status(404);
-        throw new Error(`Teacher not found`);
-    }
-
-    const removeTeacher = await db.teacher.delete({
-        where: {
-            id: findTeacher.id
-        }
-    })
-
-    return res.json(removeTeacher);
-}
