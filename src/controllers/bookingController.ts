cd44b218f92299032c2f66771b96200224c6584c
Peterdinis <peterdinis0@pobox.sk>
2023-03-04 14:19:45 +0100

    feat: Update booking

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -33,8 +33,28 @@ export const getMyBooking = async (req: Request, res: Response) => {
     return res.json(myBooking);
 }
 
-export const createBooking = (req: Request<{}, {}, createBookingType>, res: Response) => {
-    return;
+export const createBooking = async (req: Request<{}, {}, createBookingType>, res: Response) => {
+    const {from, to, username, bookId} = req.body;
+    const findBookForBorrow = await db.booking.findUnique({
+        where: {
+            id: bookId
+        }
+    })
+
+    if(!findBookForBorrow) {
+        throw new Error("Book not found")
+    }
+
+    const createNewBooking = await db.booking.create({
+        data: {
+            from,
+            to,
+            username,
+            bookId
+        }
+    })
+
+    return res.json(createNewBooking);
 }
 
 export const returnBooking = (req: Request, res: Response) => {
--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -1,6 +1,11 @@
 import express from "express";
+import { bookingInfo, createBooking, getAllBooking, getMyBooking } from "../controllers/bookingController";
 
 const router = express.Router();
 
+router.get("/bookings", getAllBooking);
+router.get("/booking/:id", bookingInfo);
+router.get("/me/booking/:username", getMyBooking);
+router.post("/booking", createBooking);
 
 export default router;
