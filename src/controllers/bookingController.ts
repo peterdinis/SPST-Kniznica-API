38f06ae2544df900bc1cf8ba2d4a7247ebf9f3a4
Peterdinis <peterdinis0@pobox.sk>
2023-03-04 11:45:49 +0100

    FOO

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -1,12 +1,24 @@
 import {Request, Response} from "express";
 import db from "../helpers/db";
 
-export const getAllBooking = (req: Request, res: Response) => {
-    return;
+export const getAllBooking = async (req: Request, res: Response) => {
+   const allBookings = await db.booking.findMany();
+   return res.json(allBookings);
 }
 
-export const bookingInfo = (req: Request, res: Response) => {
-    return;
+export const bookingInfo = async (req: Request, res: Response) => {
+    const {id} = req.params;
+    const bookInfo = await db.booking.findFirst({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    if(!bookInfo) {
+        throw new Error("Booking not found");
+    }
+
+    return res.json(bookInfo);
 }
 
 export const getMyBooking = (req: Request, res: Response) => {
