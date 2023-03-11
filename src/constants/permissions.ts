60b33512e97fa2a3ba758bbced92cd3b9b4ad96a
Peterdinis <peterdinis0@pobox.sk>
2023-03-11 19:40:33 +0100

    FOO

--- src/constants/permissions.ts
+++ src/constants/permissions.ts
@@ -1,3 +1,4 @@
 export const CAN_UPDATE = "CAN_UPDATE";
 export const CAN_CREATE = "CAN_CREATE";
 export const CAN_ADMIN = "CAN_ADMIN";
+export const CAN_DELETE = "CAN_DELETE";
--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -4,3 +4,11 @@ import db from "../helpers/db";
 export const adminExample = (req: Request, res: Response) => {
     return res.send("Admin example route");
 }
+
+export const changePermissionToTeacher = (req: Request, res: Response) => {
+    return;
+}
+
+export const removeTeacherPermission = (req: Request, res: Response) => {
+    return;
+}
