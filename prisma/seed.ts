9733ec954b29de8f60bac59f234b23322be637b8
Peterdinis <peterdinis0@pobox.sk>
2023-03-12 10:53:28 +0100

    feat: Prepare future seeder for future

---
+++ prisma/seed.ts
@@ -0,0 +1,6 @@
+import { PrismaClient, Prisma } from '@prisma/client'
+
+const prisma = new PrismaClient()
+
+const bookData: Prisma.BookCreateInput[] = [];
+const categoryData: Prisma.CategoryCreateInput[] = [];
