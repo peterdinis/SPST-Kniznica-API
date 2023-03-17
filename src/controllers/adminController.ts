aa7beca98ab7ab278690b661571e64d216a4dc0d
Peterdinis <peterdinis0@pobox.sk>
2023-03-17 18:49:24 +0100

    feat: Adding new functions to admin

--- src/controllers/adminController.ts
+++ src/controllers/adminController.ts
@@ -5,6 +5,18 @@ export const adminExample = (req: Request, res: Response) => {
     return res.send("Admin example route");
 }
 
+export const registerAdmin = (req: Request, res: Response) => {
+    return;
+}
+
+export const loginAdmin = (req: Request, res: Response) => {
+    return;
+}
+
+export const adminProfile = (req: Request, res: Response) => {
+    return;
+}
+
 export const changePermissionToTeacher = (req: Request, res: Response) => {
     return;
 }
--- src/routes/adminRoutes.ts
+++ src/routes/adminRoutes.ts
@@ -1,8 +1,10 @@
 import express from "express";
-import { adminExample } from "../controllers/adminController";
+import { adminExample, adminProfile, loginAdmin, registerAdmin } from "../controllers/adminController";
 
 const router = express.Router();
 
-router.get("/admin/example", adminExample)
-
+router.get("/admin/example", adminExample);
+router.post("/admin/register", registerAdmin);
+router.post("/admin/login", loginAdmin);
+router.get("/admin/profile", adminProfile);
 export default router;
