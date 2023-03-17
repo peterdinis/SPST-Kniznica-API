2a171ac4d16392856ee3cd831e4ed25cfc939f4b
Peterdinis <peterdinis0@pobox.sk>
2023-03-17 19:33:34 +0100

    feat: Roles

--- src/constants/permissions.ts
+++
@@ -1,4 +0,0 @@
-export const CAN_UPDATE = "CAN_UPDATE";
-export const CAN_CREATE = "CAN_CREATE";
-export const CAN_ADMIN = "CAN_ADMIN";
-export const CAN_DELETE = "CAN_DELETE";
--- src/helpers/roles.ts
+++ src/helpers/roles.ts
@@ -1,2 +1,34 @@
-import * as ac from "accesscontrol";
+import { AccessControl } from "accesscontrol";
+import { STUDENT, TEACHER } from "../constants/roleConstants";
 
+const ac = new AccessControl();
+
+ac.grant(STUDENT)
+    .readAny("book")
+    .readAny("category")
+    .create("booking")
+    .delete("booking")
+    .read("booking")
+
+
+ac.grant(TEACHER)
+    .readAny("book")
+    .readAny("category")
+    .create("book")
+    .create("category")
+    .create("booking")
+    .delete("book")
+    .delete("category")
+    .delete("booking")
+    .read("booking")
+
+    ac.grant(TEACHER)
+    .readAny("book")
+    .readAny("category")
+    .create("book")
+    .create("category")
+    .create("booking")
+    .delete("book")
+    .delete("category")
+    .delete("booking")
+    .read("booking")
