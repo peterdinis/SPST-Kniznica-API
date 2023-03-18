83b8e6310756b0e293bbc0f8d4d9d233af425601
Peterdinis <peterdinis0@pobox.sk>
2023-03-18 14:10:26 +0100

    feat: New migration

---
+++ prisma/migrations/20230318130933_adding_relation_between_author_and_book/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "Book" ADD COLUMN     "authorId" INTEGER NOT NULL DEFAULT 1;
+
+-- AddForeignKey
+ALTER TABLE "Book" ADD CONSTRAINT "Book_authorId_fkey" FOREIGN KEY ("authorId") REFERENCES "Author"("id") ON DELETE CASCADE ON UPDATE CASCADE;
