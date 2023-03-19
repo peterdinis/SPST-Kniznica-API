6553e6f8df2624ec8ac0d46f5e3487bb4dcae10f
Peterdinis <peterdinis0@pobox.sk>
2023-03-19 14:07:04 +0100

    FOO

--- src/controllers/imageController.ts
+++ src/controllers/imageController.ts
@@ -1,6 +1,21 @@
-import { Request, Response } from "request";
 import db from "../db";
+import {Request, Response} from "express";
+import fs from "fs";
 
-export const uploadImage = async (req: any, res: any) => {
-    return;
+export const uploadImage = async (req: any, res: Response) => {
+    const imageName = req.file.filename;
+    const description = req.body.description;
+
+    const addImage = await db.image.create({
+        data: {
+            filename: imageName,
+            description
+        }
+    });
+
+    return res.send(addImage);
 };
+
+export const dowloadImage = async (req: Request, res: Response) => {
+    return;
+}
