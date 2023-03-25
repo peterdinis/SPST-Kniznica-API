b7049b9eca115be917e5e7858b8f9afaf3596e26
Peterdinis <peterdinis0@pobox.sk>
2023-03-25 15:08:20 +0100

    feat: Updating seeder in backend wip

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,7 +1,40 @@
-import { PrismaClient, Prisma } from '@prisma/client'
-
+import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
+async function main() {
+  const newCategory = await prisma.category.create({
+    data: {
+        name: "Name",
+        description: "Description"
+    }
+  })
 
-const bookData: Prisma.BookCreateInput[] = [];
-const categoryData: Prisma.CategoryCreateInput[] = [];
-const authorData: Prisma.AuthorCreateInput[] = [];
+  const newAuthor = await prisma.author.create({
+    data: {
+        name: "Name",
+        birthYear: 2001,
+        country: "Slovakia",
+        description: "ROROROROR",
+        lastName: "FOFOFOFOF",
+        litPeriod: "DOROROROROR",
+    }
+  });
+
+  const newBook = await prisma.book.create({
+    data: {
+        name: "Name",
+        description: "Description",
+        image: "https://picsum.photos/200/300"
+    }
+  })
+
+  console.log(newCategory, newAuthor);
+}
+main()
+  .then(async () => {
+    await prisma.$disconnect()
+  })
+  .catch(async (e) => {
+    console.error(e)
+    await prisma.$disconnect()
+    process.exit(1)
+  })
