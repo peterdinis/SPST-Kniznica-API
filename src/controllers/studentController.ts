6dd4f1e18c645405cec05be0e141143bc6dfb906
Peterdinis <peterdinis0@pobox.sk>
2023-03-25 17:18:41 +0100

    feat: COnfirmation emails wip

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -7,6 +7,8 @@ import {
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import { getErrorMessage } from "../helpers/catchErrorMessage";
+import * as nodemailer from "nodemailer";
+import * as Mailgen from "mailgen";
 
 export const getAllStudents = async (req: Request, res: Response) => {
   const displayAllStudents = await db.student.findMany();
@@ -58,6 +60,8 @@ export const studentRegister = async (
         password: passwordHash,
       },
     });
+
+    /* TODO: Send confirm email abou registration  */
 
     return res.status(201).json(createNewStudent);
   } catch (err) {
