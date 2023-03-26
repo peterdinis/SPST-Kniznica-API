9acdcc77bc132c6fbdecf8e8e4e8db55c1504eb5
Peterdinis <peterdinis0@pobox.sk>
2023-03-26 13:40:44 +0200

    fix: Fixing bug in author controller/routes

--- src/routes/authorRoutes.ts
+++ src/routes/authorRoutes.ts
@@ -1,11 +1,11 @@
 import express from "express";
-import { createAuthor, findAllPaginatedAuthors, getAllAuthors, getOneAuthor } from "../controllers/authorController";
+import { createAuthor, findAllPaginatedAuthors, getAllAuthors, getOneAuthor} from "../controllers/authorController";
 
 const router = express.Router();
 
 router.get("/authors", getAllAuthors);
 router.get("/authors/:id", getOneAuthor);
-router.get("/authors/search", findAllPaginatedAuthors);
+router.get("/authors/paginate", findAllPaginatedAuthors);
 router.post("/authors", createAuthor);
 
 export default router;
