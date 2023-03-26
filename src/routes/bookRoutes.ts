dd3f1629451b07a04f8c0dc32326b4f97e96c3c6
Peterdinis <peterdinis0@pobox.sk>
2023-03-26 13:14:48 +0200

    FOO

--- src/routes/bookRoutes.ts
+++ src/routes/bookRoutes.ts
@@ -16,7 +16,7 @@ router.get("/books/paginate", findAllPaginatedBooks);
 router.get("/book/:id", displayOneBookFn);
 router.get("/books/search", searchBook);
 router.post("/books", createBookFn);
-router.put("/book/:id", updateBookFn);
+router.patch("/book/:id", updateBookFn);
 router.delete("/book/:id", deleteBookFn);
 
 export default router;
