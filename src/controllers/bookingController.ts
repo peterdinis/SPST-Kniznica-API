7d25a1e583757f7263c86849bfe1a68e70334c5a
Peterdinis <peterdinis0@pobox.sk>
2023-04-05 18:24:06 +0200

    feat: Adding new endpoint for booking

--- src/controllers/bookingController.ts
+++ src/controllers/bookingController.ts
@@ -2,11 +2,24 @@ import {Request, Response} from "express";
 import db from "../db";
 import { createBookingType, returnBookingType } from "../schemas/bookingSchema";
 import { AVAIABLE, NONAVAIABLE } from "../constants/bookStatus";
+import paginator from "prisma-paginate";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+const paginate = paginator(prisma);
 
 export const getAllBooking = async (req: Request, res: Response) => {
    const allBookings = await db.booking.findMany();
    return res.json(allBookings);
 }
+
+export const findAllPaginatedBooking = async (req: Request, res: Response) => {
+    const allPaginatedBooks = await paginate.booking.paginate({
+      page: Number(req.query.page) as unknown as number,
+      limit: Number(req.query.limit) as unknown as number,
+    });
+    return res.json(allPaginatedBooks);
+  };
 
 export const bookingInfo = async (req: Request, res: Response) => {
     const {id} = req.params;
