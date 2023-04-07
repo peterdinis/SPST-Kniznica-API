c39116aa5ad3d83c7a0dae0273fcfffa3d41352b
Peterdinis <peterdinis0@pobox.sk>
2023-04-07 10:17:20 +0200

    feat: Emails backend wip

---
+++ src/helpers/mailTemplate.ts
@@ -0,0 +1,47 @@
+import MailGen from "mailgen";
+import * as sgMail from "@sendgrid/mail";
+import fs from "fs";
+import { getErrorMessage } from "./catchErrorMessage";
+
+const mailGenerator = new MailGen({
+  theme: "salted",
+  product: {
+    name: "Awesome App",
+    link: "http://example.com",
+  },
+});
+
+const email = {
+  body: {
+    name: "Jon Doe",
+    intro: "Welcome to email verification",
+    action: {
+      instructions: "Please click the button below to verify your account",
+      button: {
+        color: "#33b5e5",
+        text: "Verify account",
+        link: "http://example.com/verify_account",
+      },
+    },
+  },
+};
+
+const emailTemplate = mailGenerator.generate(email);
+
+const msg = {
+  to: "your-email@example.com",
+  from: "test@example.com",
+  subject: "Test verification email",
+  html: emailTemplate,
+};
+
+fs.writeFileSync("preview.html", emailTemplate, "utf8");
+
+export const sendMail = async () => {
+  try {
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY as unknown as string);
+    return sgMail.send(msg);
+  } catch (error) {
+    getErrorMessage(error);
+  }
+};
