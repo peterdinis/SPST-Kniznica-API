a94e2922fbd427864bd104674d10a6ff3f833fe9
Peterdinis <peterdinis0@pobox.sk>
2023-04-08 12:43:11 +0200

    FOO

--- src/routes/imageRoutes.ts
+++ src/routes/imageRoutes.ts
@@ -4,6 +4,6 @@ import { upload, uploadImageForStudent } from "../controllers/imageController";
 const router = express.Router();
 
 
-router.post("/:username/upload", upload.single("file"), uploadImageForStudent);
+router.post("/student/:username/upload", upload.single("file"), uploadImageForStudent);
 
 export default router;
