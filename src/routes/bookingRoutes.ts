eb85d26a08dcc1fb3d9e60ab407af5585c1a2127
Peterdinis <peterdinis0@pobox.sk>
2023-04-09 13:30:26 +0200

    refactor: Change some backend booking endpoints urls

--- src/routes/bookingRoutes.ts
+++ src/routes/bookingRoutes.ts
@@ -13,8 +13,8 @@ const router = express.Router();
 router.get("/bookings", getAllBooking);
 router.get("/booking/paginate", findAllPaginatedBooking);
 router.get("/booking/:id", bookingInfo);
-router.get("/me/booking/:username", getMyBooking);
+router.get("/student/booking/:username", getMyBooking);
 router.post("/booking", createBooking);
-router.delete("/me/booking/return", returnBooking);
+router.delete("/student/booking/return", returnBooking);
 
 export default router;
