f31cb1b545dc74385164ad951f63d34408713810
Peterdinis <peterdinis0@pobox.sk>
2023-04-15 14:57:55 +0200

    feat: Update notification schema

--- src/validators/notificationSchema.ts
+++ src/validators/notificationSchema.ts
@@ -1,1 +1,17 @@
 import {z} from "zod";
+
+export const createNotificationSchema = z.object({
+    text: z.string({
+        required_error: "Text for notification must be defined"
+    }),
+
+    content: z.string({
+        required_error: "Content for notification must be defined"
+    }),
+
+    username: z.string({
+        required_error: "Username for student or teacher or admin must be defined"
+    })
+});
+
+export type createNotificationType = z.infer<typeof createNotificationSchema>;
