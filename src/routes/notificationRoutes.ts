4d56aeddc00ebc70249b99b42e7ae8f68c0c3a1f
Peterdinis <peterdinis0@pobox.sk>
2023-04-15 15:41:08 +0200

    FOO

--- src/routes/notificationRoutes.ts
+++ src/routes/notificationRoutes.ts
@@ -1,9 +1,10 @@
 import express from "express";
-import { getAllNotifications, notificationInfo } from "../controllers/notificationController";
+import { createNotification, getAllNotifications, notificationInfo } from "../controllers/notificationController";
 
 const router = express.Router();
 
 router.get("/notifications", getAllNotifications);
 router.get("/notification/:id", notificationInfo);
+router.post("/notification", createNotification);
 
 export default router;
