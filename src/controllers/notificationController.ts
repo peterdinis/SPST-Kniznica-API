29f52aa553d94df44f8342235ff32a66b1b2bce7
Peterdinis <peterdinis0@pobox.sk>
2023-04-15 14:25:17 +0200

    FOO

--- src/controllers/notificationController.ts
+++ src/controllers/notificationController.ts
@@ -5,3 +5,23 @@ export const getAllNotifications = async (req: Request, res: Response) => {
     const allNotifications = await db.notification.findMany();
     return res.json(allNotifications);
 }
+
+export const notificationInfo = async (req: Request, res: Response) => {
+    const {id} = req.params;
+
+    const notificationDetail = await db.notification.findFirst({
+        where: {
+            id: Number(id)
+        }
+    })
+
+    if(!notificationDetail) {
+        return res.status(404).json("Notification not found");
+    }
+
+    return res.json(notificationDetail);
+}
+
+export const createNotification = (req: Request, res: Response) => {
+    return;
+}
--- src/routes/notificationRoutes.ts
+++ src/routes/notificationRoutes.ts
@@ -1,9 +1,9 @@
 import express from "express";
-import { getAllNotifications } from "../controllers/notificationController";
+import { getAllNotifications, notificationInfo } from "../controllers/notificationController";
 
 const router = express.Router();
 
-
 router.get("/notifications", getAllNotifications);
+router.get("/notification/:id", notificationInfo);
 
 export default router;
