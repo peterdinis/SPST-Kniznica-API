924d5e757ade29dfbfb03a86ad78c285ef42d53e
Peterdinis <peterdinis0@pobox.sk>
2023-04-15 19:34:33 +0200

    FOO

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -2,7 +2,11 @@ import express from "express";
 import {
   deleteProfile,
   getAllStudents,
+  getMyNotifications,
   getStudentInfo,
+  readNotification,
+  removeAllNotifications,
+  removeNotification,
   studentLogin,
   studentProfile,
   studentRegister,
@@ -19,5 +23,18 @@ router.post("/student/login", studentLogin);
 router.get("/student/profile", verifyToken, studentProfile);
 router.patch("/student/profile/update/:id", updateProfile);
 router.delete("/student/delete/:id", deleteProfile);
+router.get("/student/:username/notifications", getMyNotifications);
+router.patch(
+  "/student/:username/notification/:notificationId/read",
+  readNotification
+);
+router.delete(
+  "/student/:username/notification/:notificationId/remove",
+  removeNotification
+);
+router.delete(
+  "/student/:username/notifications/remove/all",
+  removeAllNotifications
+);
 
 export default router;
