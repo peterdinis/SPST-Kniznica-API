2bc5a22cdfe04d005078636e078d0bcadcead3a5
Peterdinis <peterdinis0@pobox.sk>
2023-04-16 18:06:02 +0200

    feat: Adding new teacher routes

--- src/routes/teacherRoutes.ts
+++ src/routes/teacherRoutes.ts
@@ -2,7 +2,11 @@ import express from "express";
 import { verifyToken } from "../middleware/auth";
 import {
   getAllTeachers,
+  getMyNotifications,
   getTeacherInfo,
+  readNotification,
+  removeAllNotifications,
+  removeNotification,
   teacherLogin,
   teacherProfile,
   teacherRegister,
@@ -15,5 +19,17 @@ router.get("/teacher/info/:id", getTeacherInfo);
 router.post("/teacher/register", teacherRegister);
 router.post("/teacher/login", teacherLogin);
 router.get("/teacher/profile", verifyToken, teacherProfile);
-
+router.get("/teacher/:username/notifications", getMyNotifications);
+router.patch(
+  "/teacher/:username/notification/:notificationId/read",
+  readNotification
+);
+router.delete(
+  "/teacher/:username/notification/:notificationId/remove",
+  removeNotification
+);
+router.delete(
+  "/teacher/:username/notifications/remove/all",
+  removeAllNotifications
+);
 export default router;
