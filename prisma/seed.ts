197efd961e7694b0c9667b021ee716f14196e837
Peterdinis <peterdinis0@pobox.sk>
2023-04-21 18:31:43 +0200

    feat: Updating seeder

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,12 +1,12 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 import bcrypt from "bcrypt";
-import { ADMIN, STUDENT, TEACHER } from '../src/constants/roleConstants';
+import { ADMIN, STUDENT, TEACHER } from "../src/constants/roleConstants";
 
 const prisma = new PrismaClient();
 
 const studentPassword = "testPassword";
 const teacherPassword = "testTeacherPassword";
-const adminPassword = "testAdminPassword"
+const adminPassword = "testAdminPassword";
 
 const hashedStudentPassword = bcrypt.hash(studentPassword, 12);
 const hashedTeacherPassword = bcrypt.hash(teacherPassword, 12);
@@ -15,35 +15,61 @@ const hashedAdminPassword = bcrypt.hash(adminPassword, 12);
 async function main() {
   const newCategory = await prisma.category.create({
     data: {
-        name: "Name1",
-        description: "Description"
-    }
-  })
+      name: "Name1",
+      description: "Description",
+    },
+  });
 
   const newAuthor = await prisma.author.create({
     data: {
-        name: "Name1",
-        birthYear: 2001,
-        country: "Slovakia",
-        description: "ROROROROR",
-        lastName: "FOFOFOFOF",
-        litPeriod: "DOROROROROR",
-    }
+      name: "Name1",
+      birthYear: 2001,
+      country: "Slovakia",
+      description: "ROROROROR",
+      lastName: "FOFOFOFOF",
+      litPeriod: "DOROROROROR",
+    },
   });
 
   const newBook = await prisma.book.create({
     data: {
-        name: "Name",
-        description: "Description",
-        image: "https://picsum.photos/200/300",
-        pages: 20202,
-        status: "Dostupná",
-        authorId: 1,
-        categoryId: 1,
-        year: 2020
-    }
-  })
-  
+      name: "Name",
+      description: "Description",
+      image: "https://picsum.photos/200/300",
+      pages: 20202,
+      status: "Dostupná",
+      authorId: 1,
+      categoryId: 1,
+      year: 2020,
+    },
+  });
+
+  const secondBook = await prisma.book.create({
+    data: {
+      name: "Name1",
+      description: "Description",
+      image: "https://picsum.photos/200/300",
+      pages: 20202,
+      status: "Dostupná",
+      authorId: 1,
+      categoryId: 1,
+      year: 2020,
+    },
+  });
+
+  const thirdBook = await prisma.book.create({
+    data: {
+      name: "Name2",
+      description: "Description",
+      image: "https://picsum.photos/200/300",
+      pages: 20202,
+      status: "Dostupná",
+      authorId: 1,
+      categoryId: 1,
+      year: 2020,
+    },
+  });
+
   const newStudent = await prisma.student.create({
     data: {
       email: "testStudent@gmail.com",
@@ -53,8 +79,8 @@ async function main() {
       username: "TestStudent",
       name: "RIRIRIR",
       role: STUDENT,
-      picture: null
-    }
+      picture: null,
+    },
   });
 
   const newTeacher = await prisma.teacher.create({
@@ -65,7 +91,7 @@ async function main() {
       name: "Tester",
       username: "TestTeacher",
       role: TEACHER,
-    }
+    },
   });
 
   const newAdmin = await prisma.admin.create({
@@ -75,35 +101,45 @@ async function main() {
       name: "Admin",
       lastName: "TestAdmin",
       role: ADMIN,
-      username: "testAdmin"
-    }
+      username: "testAdmin",
+    },
   });
-
 
   const newNotification = await prisma.notification.create({
     data: {
       content: "ororororororo",
       username: "TestStudent",
       isRead: true,
-      text: "Create new booking"
-    }
+      text: "Create new booking",
+    },
   });
 
   const newMessage = await prisma.message.create({
     data: {
       description: "Custom description",
-      name: "Custom Name"
-    }
-  })
+      name: "Custom Name",
+    },
+  });
 
-  console.log(newBook, newCategory, newAuthor, newAdmin, newMessage, newTeacher, newStudent, newNotification);
+  console.log(
+    newBook,
+    newCategory,
+    newAuthor,
+    newAdmin,
+    newMessage,
+    newTeacher,
+    newStudent,
+    newNotification,
+    secondBook,
+    thirdBook
+  );
 }
 main()
   .then(async () => {
-    await prisma.$disconnect()
+    await prisma.$disconnect();
   })
   .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+    console.error(e);
+    await prisma.$disconnect();
+    process.exit(1);
+  });
