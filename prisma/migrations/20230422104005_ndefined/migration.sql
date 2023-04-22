bdc00acc8e83b6ebd86a425fdf362e2b09c98656
Peterdinis <peterdinis0@pobox.sk>
2023-04-22 12:40:18 +0200

    fix: Fixing external ids columns wip

---
+++ prisma/migrations/20230422104005_ndefined/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Author" ALTER COLUMN "externalId" SET DEFAULT '102038494';
+
+-- AlterTable
+ALTER TABLE "Book" ALTER COLUMN "externalId" SET DEFAULT '1010';
+
+-- AlterTable
+ALTER TABLE "Category" ALTER COLUMN "externalId" SET DEFAULT '10202';
