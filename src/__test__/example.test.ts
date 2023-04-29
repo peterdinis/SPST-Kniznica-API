c186957b63418ba6ff0212e2ac472b2698947121
Peterdinis <peterdinis0@pobox.sk>
2023-04-29 09:59:36 +0200

    FOO

--- src/__test__/example.test.ts
+++ src/__test__/example.test.ts
@@ -1,9 +1,11 @@
 import { app } from "../server";
 import request from "supertest";
 
-describe("Testing example endpoint", async () => {
-  const exampleRequest = await request(app).get("/example").expect(200);
+describe("Testing example endpoint", () => {
+  it("Testing example endpoint", async () => {
+    const exampleRequest = await request(app).get("/example").expect(200);
 
-  const exampleResponse = exampleRequest.body;
-  expect(exampleResponse).toBe("OK");
+    const exampleResponse = exampleRequest.body;
+    expect(exampleResponse).toBe("OK");
+  });
 });
