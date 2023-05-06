3b60535e1776a243fe1befd04da7269683280c72
Peterdinis <peterdinis0@pobox.sk>
2023-05-06 09:28:05 +0200

    FOO

--- src/middleware/multer.ts
+++ src/middleware/multer.ts
@@ -3,7 +3,7 @@ import multer from "multer";
 export const upload = multer({
   storage: multer.diskStorage({
     destination: (req, file, cb) => {
-      cb(null, "./uploads");
+      cb(null, "C:/Users/Peter/Desktop/node/spst-kniznica-api/uploads");
     },
     filename: (req, file, cb) => {
       cb(null, `${Date.now()}-${file.originalname}`);
