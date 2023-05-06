6d9d31c886791206c345371521da204bd35753a1
Peterdinis <peterdinis0@pobox.sk>
2023-05-06 12:43:58 +0200

    feat: Updating backend tests wip

---
+++ src/__test__/book.test.ts
@@ -0,0 +1,10 @@
+import { app } from "../server";
+import request from "supertest";
+
+describe("Testing Books Endpoints", () => {
+    it.only("Get All Books", async () => {
+        const allBooksRequest = await request(app).get("/books");
+        const allBooksResponse = allBooksRequest.body;
+        console.log(allBooksResponse);
+    })
+})
--- src/interfaces/IUser.ts
+++
@@ -1,6 +0,0 @@
-export interface IUser {
-    id: string;
-    socketId: string;
-    name: string;
-    email: string;
-  };
