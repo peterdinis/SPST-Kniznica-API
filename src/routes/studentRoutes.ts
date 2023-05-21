d058325dabce450f59e81c0f50f4ad2e210da185
Peterdinis <peterdinis0@pobox.sk>
2023-05-21 11:57:32 +0200

    fix: Update params in two student endpoints

--- src/routes/studentRoutes.ts
+++ src/routes/studentRoutes.ts
@@ -19,8 +19,8 @@ router.get("/student/:paginate", findAllPaginatedStudents);
 router.post("/student/register", studentRegister);
 router.post("/student/login", studentLogin);
 router.get("/student/profile", verifyToken, studentProfile);
-router.patch("/student/profile/update/:id", updateProfile);
-router.delete("/student/profile/delete/:id", deleteProfile);
+router.patch("/student/profile/update/:username", updateProfile);
+router.delete("/student/profile/delete/:username", deleteProfile);
 
 
 export default router;
