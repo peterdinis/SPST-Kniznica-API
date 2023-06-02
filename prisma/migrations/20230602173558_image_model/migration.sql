91ee5aa299d191946555f0afbb9a40e1458ca2c9
Peterdinis <peterdinis0@pobox.sk>
2023-06-02 19:36:08 +0200

    feat: Adding new model

---
+++ prisma/migrations/20230602173558_image_model/migration.sql
@@ -0,0 +1,10 @@
+-- CreateTable
+CREATE TABLE "Image" (
+    "id" SERIAL NOT NULL,
+    "filename" TEXT NOT NULL,
+    "path" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Image_pkey" PRIMARY KEY ("id")
+);
