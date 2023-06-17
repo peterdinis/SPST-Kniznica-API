b4069552815480609e09ff837ae0caae11ced71f
Peterdinis <peterdinis0@pobox.sk>
2023-06-17 12:28:45 +0200

    fix: Fixing bugs in job

--- src/cron/bookCron.ts
+++ src/cron/bookCron.ts
@@ -1,6 +1,7 @@
 import db from "../db";
 import cron from "node-cron";
 import { getErrorMessage } from "../helpers/catchErrorMessage";
+import { DateTime } from 'luxon';
 
 const date = new Date();
 
@@ -23,5 +24,11 @@ export async function displayBooksOrderedByCreationDate() {
 // Schedule the job to run at your desired interval
 cron.schedule('0 9 * * *', async () => {
   console.log('Running job to display books ordered by creation date...');
+
+  // Convert current date and time to the specified timezone
+  const londonTime = DateTime.now().setZone('Europe/London');
+
+  console.log(`Current time in London: ${londonTime.toISO()}`);
+
   await displayBooksOrderedByCreationDate();
-}, { scheduled: true, timezone: londonTime });
+}, { scheduled: true });
