d2d275b4bd566fee357299dd531ae864e078a07a
Peterdinis <peterdinis0@pobox.sk>
2023-06-17 09:39:28 +0200

    FOO

--- src/cron/bookingCron.ts
+++ src/cron/bookingCron.ts
@@ -1,2 +1,4 @@
 import db from "../db";
 import cron from "node-cron";
+
+/* Job ktorý kontroluje dátum požičania default 7 dní ak ten dátum presiahol 7 dní upozorní používateľa že má knihu vrátiť */
