cf7b11b91a9e7b385bcefa7325f938c1765b58e2
Peterdinis <peterdinis0@pobox.sk>
2023-06-18 08:36:52 +0200

    FOO

--- src/db.ts
+++ src/db.ts
@@ -2,40 +2,4 @@ import { PrismaClient } from "@prisma/client";
 
 const db = new PrismaClient();
 
-db.$use(async (params, next) => {
-  if (params.model === "Book" && params.action === "update") {
-    params.args.data.updatedAt = new Date();
-  }
-
-  if (params.model === "Category" && params.action === "update") {
-    params.args.data.updatedAt = new Date();
-  }
-
-  if (params.model === "Student" && params.action === "update") {
-    params.args.data.updatedAt = new Date();
-  }
-
-  if (params.model === "Teacher" && params.action === "update") {
-    params.args.data.updatedAt = new Date();
-  }
-
-  if (params.model === "Admin" && params.action === "update") {
-    params.args.data.updatedAt = new Date();
-  }
-
-  if (params.model === "Booking" && params.action === "update") {
-    params.args.data.updatedAt = new Date();
-  }
-
-  if (params.model === "Author" && params.action === "update") {
-    params.args.data.updatedAt = new Date();
-  }
-
-  if (params.model === "Message" && params.action === "update") {
-    params.args.data.updatedAt = new Date();
-  }
-
-  return next(params);
-});
-
 export default db;
