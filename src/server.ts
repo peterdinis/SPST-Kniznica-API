7ebb9a83e691c2253fa900247b93547b1c458ff7
Peterdinis <peterdinis0@pobox.sk>
2023-07-05 11:54:06 +0200

    feat: Removing messages

--- src/server.ts
+++ src/server.ts
@@ -13,7 +13,6 @@ import adminRoutes from "./routes/adminRoutes";
 import compression from "compression";
 import authorRoutes from "./routes/authorRoutes";
 import errorHandler from "errorhandler";
-import messageRoutes from "./routes/messageRoutes";
 
 export const app: Application = express();
 
@@ -44,7 +43,6 @@ app.use(studentRoutes);
 app.use(bookingRoutes);
 app.use(adminRoutes);
 app.use(authorRoutes);
-app.use(messageRoutes);
 
 app.listen(PORT, () => {
     console.log(`Applikácia beží na porte ${PORT}`)
