ad93dd9ff3eb93bd74d129e8956d595bd92f81b4
Peterdinis <peterdinis0@pobox.sk>
2023-07-14 19:27:49 +0200

    feat: Update get author detail fn

--- src/controllers/authorController.ts
+++ src/controllers/authorController.ts
@@ -39,6 +39,9 @@ export const getOneAuthor = async (req: Request, res: Response) => {
       where: {
         externalId: Number(externalId),
       },
+      include: {
+        books: true
+      }
     });
 
     if (!oneAuthor) {
