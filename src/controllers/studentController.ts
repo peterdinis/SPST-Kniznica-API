2d9ad852773780ef67b5e747af59184a1f46cc4a
Peterdinis <peterdinis0@pobox.sk>
2023-07-22 18:00:22 +0200

    fix: Fixing upload code wip

--- src/controllers/studentController.ts
+++ src/controllers/studentController.ts
@@ -168,13 +168,16 @@ export const uploadPicture = async (req: Request, res: Response) => {
     }
 
     const avatar = req.file?.buffer || null; // Get the image buffer from multer
+    console.log(avatar);
 
     const updatedStudent = await prisma.student.update({
       where: { id: parseInt(id) },
       data: { picture: avatar },
     });
 
-    res.json(updatedStudent);
+    console.log(updatedStudent)
+
+    return res.json(201).json("Fotka bola nahraná");
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: "Something went wrong" });
