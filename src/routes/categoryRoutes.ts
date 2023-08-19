e6cdcc7e23242e1a0f534b5d0311abd75f84a15f
Peterdinis <peterdinis0@pobox.sk>
2023-08-19 10:02:12 +0200

    feat: Update update cateogry http method

--- src/routes/categoryRoutes.ts
+++ src/routes/categoryRoutes.ts
@@ -16,7 +16,7 @@ router.get("/categories/paginate", findAllPaginatedCategoriesFn);
 router.get("/category/search", searchForCategory);
 router.get("/category/:externalId", categoryDetailsFn);
 router.post("/category", createCategoryFn);
-router.patch("/category/:id",  updateCategory);
+router.put("/category/:id",  updateCategory);
 router.delete("/category/:id", deleteCategory);
 
 export default router;
