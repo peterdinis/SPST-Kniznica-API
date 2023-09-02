f9c6ad39e7ce324787191c4027a1b21e4b65718a
Peterdinis <peterdinis0@pobox.sk>
2023-09-02 19:49:30 +0200

    feat: Adding baseEntity

--- src/db.ts
+++ src/db.ts
@@ -3,11 +3,11 @@ import { DataSource } from "typeorm"
 export const myDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
-    port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
-    entities: ["src/entities/entity/*.ts"],
+    port: process.env.PORT as unknown as number,
+    username: process.env.USERNAME as unknown as string,
+    password: process.env.PASSWORD as unknown as string,
+    database: process.env.DATABASE as unknown as string,
+    entities: [],
     logging: true,
     synchronize: true,
 })
--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
+import "reflect-metadata";
 import express, { Application } from "express";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -12,38 +13,46 @@ import adminRoutes from "./routes/adminRoutes";
 import compression from "compression";
 import authorRoutes from "./routes/authorRoutes";
 import errorHandler from "errorhandler";
+import { myDataSource } from "./db";
 
-const app: Application = express();
+myDataSource
+  .initialize()
+  .then(() => {
+    const app: Application = express();
 
-if (process.env.NODE_ENV === "development") {
-  app.use(errorHandler());
-}
+    if (process.env.NODE_ENV === "development") {
+      app.use(errorHandler());
+    }
 
-app.use(
-  cors({
-    origin: "*",
-    allowedHeaders: "*",
-    methods: "*",
-    exposedHeaders: "*",
+    app.use(
+      cors({
+        origin: "*",
+        allowedHeaders: "*",
+        methods: "*",
+        exposedHeaders: "*",
+      })
+    );
+
+    app.use(compression());
+    app.use(express.json());
+    app.use(morgan("dev"));
+    app.use(helmet());
+    dotenv.config();
+
+    const PORT = (process.env.PORT as unknown as number) || 8111;
+
+    app.use(bookRoutes);
+    app.use(categoryRoutes);
+    app.use(teacherRoutes);
+    app.use(studentRoutes);
+    app.use(bookingRoutes);
+    app.use(adminRoutes);
+    app.use(authorRoutes);
+
+    app.listen(PORT, () => {
+      console.log(`Applikácia beží na porte ${PORT}`);
+    });
   })
-);
-
-app.use(compression());
-app.use(express.json());
-app.use(morgan("dev"));
-app.use(helmet());
-dotenv.config();
-
-const PORT = (process.env.PORT as unknown as number) || 8111;
-
-app.use(bookRoutes);
-app.use(categoryRoutes);
-app.use(teacherRoutes);
-app.use(studentRoutes);
-app.use(bookingRoutes);
-app.use(adminRoutes);
-app.use(authorRoutes);
-
-app.listen(PORT, () => {
-  console.log(`Applikácia beží na porte ${PORT}`);
-});
+  .catch((err) => {
+    console.log(err);
+  });
