826f23698523f42e72eed28a1bb8f125cf198f26
Peterdinis <peterdinis0@pobox.sk>
2023-09-03 15:55:52 +0200

    feat: Update tsconfig.json

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,10 @@
 import { DataSource } from "typeorm"
+import { Book } from "./entities/bookEntity"
+import { Category } from "./entities/categoryEntity"
+import { Teacher } from "./entities/teacherEntity"
+import { Student } from "./entities/studentEntity"
+import { Booking } from "./entities/bookingEntity"
+import { Author } from "./entities/authorEntity"
 
 export const myDataSource = new DataSource({
     type: "mysql",
@@ -7,7 +13,7 @@ export const myDataSource = new DataSource({
     username: process.env.USERNAME as unknown as string,
     password: process.env.PASSWORD as unknown as string,
     database: process.env.DATABASE as unknown as string,
-    entities: [],
+    entities: [Book, Category, Teacher, Student, Booking, Author],
     logging: true,
     synchronize: true,
 })
