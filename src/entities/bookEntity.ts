2faedfd5861eebe2f269b819b95a9c9a5d063ad2
Peterdinis <peterdinis0@pobox.sk>
2023-09-14 21:00:28 +0200

    feat: Updating entities WIP

--- src/entities/bookEntity.ts
+++ src/entities/bookEntity.ts
@@ -1,6 +1,58 @@
-import { BaseEntity, Entity } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, ManyToOne, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Category } from './Category';
+import { Author } from './Author'; 
+import { Booking } from './Booking';
 
 @Entity()
 export class Book extends BaseEntity {
-    
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: 1 })
+  externalId: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  image: string;
+
+  @Column()
+  status: string;
+
+  @Column({ default: 'Mladé Léta' })
+  publisher: string;
+
+  @Column()
+  pages: number;
+
+  @Column()
+  year: number;
+
+  @Column({ default: 1 })
+  quantity: number;
+
+  @Column({ default: 1 })
+  categoryId: number;
+
+  @ManyToOne(() => Category, (category) => category.books, { onDelete: 'CASCADE' })
+  category: Category;
+
+  @Column()
+  authorId: number;
+
+  @ManyToOne(() => Author, (author) => author.books, { onDelete: 'CASCADE' })
+  author: Author;
+
+  @OneToMany(() => Booking, (booking) => booking.book)
+  borrowed: Booking[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
--- src/entities/categoryEntity.ts
+++ src/entities/categoryEntity.ts
@@ -1,6 +1,26 @@
-import { BaseEntity, Entity } from "typeorm";
+import { Entity, PrimaryGeneratedColumn, Column, BaseEntity, OneToMany, CreateDateColumn, UpdateDateColumn } from 'typeorm';
+import { Book } from "./bookEntity"
 
 @Entity()
 export class Category extends BaseEntity {
-    
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column({ default: 1 })
+  externalId: number;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @OneToMany(() => Book, (book) => book.category)
+  books: Book[];
+
+  @CreateDateColumn()
+  createdAt: Date;
+
+  @UpdateDateColumn()
+  updatedAt: Date;
 }
