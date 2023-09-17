a08ded90a0aaef4e5c09b54aa5f732f3cd8778bd
Peterdinis <peterdinis0@pobox.sk>
2023-09-17 17:17:26 +0200

    fix: Fixing crucial bug with prisma

--- src/db.ts
+++ src/db.ts
@@ -1,5 +1,6 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from '@prisma/client'
 
-const db = new PrismaClient();
+const db = global.prismadb || new PrismaClient()
+if (process.env.NODE_ENV !== 'production') global.prismadb = db
 
-export default db;
+export default db
